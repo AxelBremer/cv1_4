ccb3735e86fc79559022d76f9a52bd8c1eeef00a
AxelBremer <axelbremer94@gmail.com>
2019-03-11 14:24:32 +0100

    keypoint matching done

--- keypoint_matching.m
+++ keypoint_matching.m
@@ -1,11 +1,32 @@
-function keypoints = keypoint_matching(im1, im2)
+function keypoint_matches = keypoint_matching(im1, im2, print)
+    set_size = 10
     [f1,d1] = vl_sift(im1);
-    imshow(im1);
-    hold on;
-    perm = randperm(size(f1,2));
-    sel = perm(1:50) ;
-    h1 = vl_plotframe(f1(:,sel)) ;
-    h2 = vl_plotframe(f1(:,sel)) ;
-    set(h1,'color','k','linewidth',3) ;
-    set(h2,'color','y','linewidth',2) ;
+    [f2,d2] = vl_sift(im2);
+    [matches, scores] = vl_ubcmatch(d1, d2);
+    
+    matches1 = f1(1:2,matches(1,:));
+    matches2 = f2(1:2,matches(2,:));
+    keypoint_matches = cat(1,matches1,matches2);
+    
+    if print == 1
+        perm = randperm(size(matches,2));
+        sel = perm(1:set_size);
+        m_sel = matches(:,sel);
+        s_sel = scores(:,sel);
+        
+        im_width = size(im1,2);
+        big_im = cat(2,im1,im2);
+        imshow(big_im);
+        hold on;
+
+        for i = 1:set_size
+            match = m_sel(:,i);
+            f_1 = f1(:,match(1));
+            f_2 = f2(:,match(2));
+            f_2(1,:) = f_2(1,:) + im_width;
+            h1 = vl_plotframe(f_1);
+            h1 = vl_plotframe(f_2);
+            plot([f_1(1,:) f_2(1,:)],[f_1(2,:) f_2(2,:)]);
+        end
+    end
 end
--- kp_matching_demo.m
+++ kp_matching_demo.m
@@ -1,4 +1,4 @@
 im1 = im2single(imread('boat1.pgm'));
 im2 = im2single(imread('boat2.pgm'));
 
-keypoint_matching(im1, im2);
+keypoint_matching(im1, im2, 1);
